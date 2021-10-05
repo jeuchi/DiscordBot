db85a3b048b269b07f48a58d4dce159f20323587
Jeremy Euchi <jeuchi@icloud.com>
2021-10-04 19:30:01 -0700

    Authentification succesful
    Able to GET/POST to Ubisoft

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # bot.py
 import os
 import random
+import time
 
 import discord
 from ubi import UbiAuth
@@ -18,22 +19,19 @@ ASK_RESP = ['Outlook is trash bruh',
             'Maybe in a different universe'
 ]
 
+cooldown = 0
+
+# Ubi authenticate
+email = os.getenv('UBIEMAIL')
+password = os.getenv('UBIPASS')
+ubi = UbiAuth(email=email, password=password)
+
 client = commands.Bot(command_prefix='.')
 
 @client.event
 async def on_ready():
-  print(f'{client.user.name} has connected to Discord!')
-
-  # Ubi authenticate
-  email = os.getenv('UBIEMAIL')
-  password = os.getenv('UBIPASS')
-  ubi = UbiAuth(email=email, password=password)
-  user = "SecretingJuicer"
-  link = f'https://public-ubiservices.ubi.com/v1/spaces/5172a557-50b5-4665-b7db-e3f2e8c5041d/sandboxes/OSBOR_PC_LNCH_A/r6karma/players?board_id=pvp_ranked&season_id=-1&region_id=ncsa&profile_ids={user}'
-
-  #ubi.ticket = "t=ewogICJ2ZXIiOiAiMSIsCiAgImFpZCI6ICIzNTg3ZGNiYi03ZjgxLTQ1N2MtOTc4MS0wZTNmMjlmNmY1NmEiLAogICJlbnYiOiAiUHJvZCIsCiAgInNpZCI6ICJhZDkwYTI0YS02MjU4LTQzNjMtYjdlMi0yYjM5YzQyYWZmMWYiLAogICJ0eXAiOiAiSldFIiwKICAiZW5jIjogIkExMjhDQkMiLAogICJpdiI6ICItLWdTeFBTdFZqTjktYS00d3gyMVRRIiwKICAiaW50IjogIkhTMjU2IiwKICAia2lkIjogIjRjYmVhMzE1LWNlMmMtNGVmOS04MjM3LTc5ZjgyN2U5ZjRmOCIKfQ.brortHyBDIKc8p0m9JdrQyYsFDIBJTA9sHYLeYNxX_5IbbN9rY_w218nGhT8AjTiifeSr7Pjyye6dsZzOQ86GpC8vKLtVI2JAHwSn9Ys702P2uBRrJO652-LIGyvrrs8_HFHeE8jhUNSS4Q0_yRce1g_e9AuL9YOwi1qdrwXMLCdbmtqKRQIkY4d4UbqKT3ClhDyvmDJ8qzcO6fcL0aP5DavTIKakuv24IelALQ9mHtLjQUERB3e9cpv67Z4x4CzE8aYPPqNs2O_BUYNKWXnwe9zwwh_d7-Cp0M2U8iZm20YyXyTx_HHZcRBMMcqedUlVQCL9am_4I97xV0lXzUngqNDKeAOJsUaxvZVjeczkYygMRvASZjLq7x7Ky5gZHknNPKT_bZQHgJ2TyyD_usFEYfzKz8SoiP0pfYPA7HGhuNJgc_VDEuh2jZtDWsAewW8AL12ZKgJDtS2sbPp2svn0U-IoNJxD1nbAwqc4LnNCuRpM4yFm65JIRV5hdvGKFqC2aaUQDdvaqi7hRRF-pxRuAMcO0y17i02ePlQdn79kzdm8dKhP5rmwIBu5vfEqDNeOqJ_IEE_NOjhw3ShtekBe9pQN1hnM1gYdrqWowABubNaAzZNeaU_Dk22LPXhWQMiHfUpXPC67leA2IHrEoGcoD1vDaJndDDVsTPNYimPDeyyGMEZ5AFdQlNnAeVjBwZGfi0XgBHGAYIbS_EApacSvxkC9PiKslrNrTmN09ff9NJLafSKengH2SowRgejiNAQiOfi0EpnCptJ9ZGEWu28zO-mpUELPdtxI5sfQRQN9Ldzv4KPLanjxer96jd9gSIiXLkocyGiaUdbFAxo7DztmZ5Vx4EjLL0_NySXEcvOBsI9ruxGCIbl0RU0PgD9QKLgTAuQhh0KOUidhslCPwx-qDnj7_2IzHCpBj5BET6xHD4Rd3UyrBedETKJyDtnqmXXrRzhAQcfz0hjfwHs_TaTdhQ4vsKzrpeDiNWykoMoxwIkTcZV9Kv-qndNEysC_kcJ4K6yzktIMTD9ipmj2o9b4VQWfFg-5pjzEpXutlq43136aCd7ABUqNJySHkiMNLUbSxcsj0SIHWCvPMZsLuZnxlA4P2vYeyuXS6XuqGAZh_I.yVThZidT73On2I8rsMH7k_4vOXuTH20DP7cwyRCTxsU"
-  #ubi.sessionId="ad90a24a-6258-4363-b7e2-2b39c42aff1f"
-  #await ubi.getData(string)
+  global cooldown
+  cooldown = time.time()
 
 @client.event
 async def on_member_join(member):
@@ -41,7 +39,6 @@ async def on_member_join(member):
   await member.dm_channel.send(
       f'Hi {member.name}, you can go pog yourself!'
   )
-
 
 @client.command()
 async def ask(ctx, *, question):
@@ -51,8 +48,43 @@ async def ask(ctx, *, question):
 async def clear(ctx, amount=10):
   await ctx.channel.purge(limit=amount)
 
+# R6 command
+#   MMR    
+#   KD ratio seasonal
 @client.command()
-async def r6(ctx, user):
-  return 
+async def r6(ctx, username):
+  global cooldown
+  remaining_time = int(time.time() - cooldown)
+  if remaining_time < 10:
+    remaining_time = 10 - remaining_time
+    await ctx.send(f'Cooldown, wait {remaining_time} second(s)')
+    return False
 
+  cooldown = time.time()
+
+  link = f'https://public-ubiservices.ubi.com/v3/profiles?namesOnPlatform={username}&platformType=uplay'
+  resp_json = ubi.getData(link)
+
+  userId = resp_json['profiles'][0]['userId']
+  link = f'https://public-ubiservices.ubi.com/v1/spaces/5172a557-50b5-4665-b7db-e3f2e8c5041d/sandboxes/OSBOR_PC_LNCH_A/r6karma/players?board_id=pvp_ranked&season_id=-1&region_id=ncsa&profile_ids={userId}'
+
+  resp_json = ubi.getData(link)
+
+  mmr = resp_json['players'][userId]['mmr']
+  kills = resp_json['players'][userId]['kills']
+  deaths = resp_json['players'][userId]['deaths']
+
+  kd_ratio_seasonal =  (kills / deaths)
+  kd_ratio_seasonal_str = '{:.2f}'.format(kd_ratio_seasonal)
+
+  if (mmr < 3200):
+    await ctx.send(f'[MMR - {mmr}] LOL NOT EVENT PLAT 3')
+  else:
+    await ctx.send(f'[MMR - {mmr}] Not bad!')
+
+  if (kd_ratio_seasonal >= 1):
+    await ctx.send(f'[KD Seasonal - {kd_ratio_seasonal_str}] EZ Clap')
+  else:
+    await ctx.send(f'[KD Seasonal - {kd_ratio_seasonal_str}] Always dead I guess...')
+   
 client.run(TOKEN)
--- ubi.py
+++ ubi.py
@@ -4,11 +4,12 @@ import os
 from datetime import datetime, timedelta
 
 import requests
+from requests.api import head
 
 class UbiAuth:
   # Initialize token authentication from email/password and save session.
   #   Email: User's Ubisoft email.
-  #   Password: User's Ubisoft password
+  #   Password: User's Ubisoft password.
   def __init__(self, email, password):
     self.token = base64.b64encode((email + ":" + password).encode("utf-8")).decode("utf-8")
     self.appId = '3587dcbb-7f81-457c-9781-0e3f29f6f56a'
@@ -17,24 +18,29 @@ class UbiAuth:
   # Request using authentication and save session token for use in retrieving Ubisoft profiles.
   def getUbiData(self):
     headers = {
+            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
 			'Content-Type': 'application/json',
-			'Authorization': 'Basic' + self.token,
-			'Ubi-AppId': self.appId
+			'Authorization': 'Basic ' + self.token,
+			'Ubi-AppId': self.appId,
+            'Connection': 'keep-alive'
 		}
 
     payload = {
       'rememberMe': 'true'
     }
 
-    resp_raw = requests.post('https://public-ubiservices.ubi.com/v3/profiles/sessions', headers=headers, data=payload)
+    resp_raw = requests.post('https://public-ubiservices.ubi.com/v3/profiles/sessions', headers=headers, json=payload)
 
+    if (resp_raw is None):
+        print("Ubisoft authentification did not return a response")
+        return False
+    
     resp_json = resp_raw.json()
-    httpCode = resp_json['httpCode']
 
-    print(resp_json)
-    if httpCode != 200:
-      print(f'Ubisoft authentication failed with HTTP code: {httpCode}')
-      return False
+    if resp_raw.status_code != 200:
+        print(resp_json)
+        print(f'Ubisoft authentication failed with HTTP code: {resp_raw.status_code}')
+        return False
       
     self.ticket = resp_json['ticket']
     self.sessionId = resp_json['sessionId']
@@ -42,8 +48,9 @@ class UbiAuth:
     self.spaceId = resp_json['spaceId']
     self.ownUserId = resp_json['userId']
   
-  async def getData(self, link):
+  def getData(self, link):
     headers = {
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
       'Content-Type': 'application/json',
       'Authorization': 'Ubi_v1 t=' + self.ticket,
       'Ubi-AppId': self.appId,
@@ -53,15 +60,7 @@ class UbiAuth:
     }
 
     if link:
-      async with aiohttp.ClientSession() as session:
-        async with session.get(link, headers=headers) as r:
-          
-          if r.status == 200:
-            return await r.json()
-          else:
-            print(f'{Fore.RED}[Error]{Style.RESET_ALL} An error occured: ')
-            print(r.reason)
-            print(r)
-            return False
+        resp_raw = requests.get(link, headers=headers)
+        return resp_raw.json()
     else:
-      return False
+        return False

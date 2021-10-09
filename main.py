d2f7e8cee77d648ec1296f264ee41aea8b71e784
Jeremy Euchi <jeuchi@icloud.com>
2021-10-09 13:58:47 -0700

    Music and R6 rework
    Add music player and isolated music and r6 comands.
    Add constants file for convenience.

--- main.py
+++ main.py
@@ -4,149 +4,49 @@
 # Description: Pepega bot used in private server. 
 # Commands to print Rainbow Six Siege stats and ask questions.
 
-import os
 import random
 import time
-import discord 
-from ubi import UbiAuth, Player
+import discord
+from music import Music
+from discord.ext.commands.core import after_invoke 
+from ubi import R6
 from discord.ext import commands
 from dotenv import load_dotenv
+from constants import ASK_RESP, NAME, VERSION, TOKEN
 
 load_dotenv()
 
-# Globals.
-TOKEN = os.getenv('DISCORD_TOKEN')
-
-ASK_RESP = ['Outlook is trash bruh',
-            'Yeah...idk cuh',
-            'LOOKING POGGERS DUD',
-            'N <:OMEGALUL:792923100931948585>',
-            'Maybe? stfu leave me alone',
-            'YEP',
-            'No and ur ugly + ratio + L',
-            'Maybe in a different universe'
-]
-
-EMOTES = ['https://cdn.discordapp.com/emojis/894659685049856100.png?size=96', # WeirdChamp
-          'https://cdn.discordapp.com/emojis/893378054695559188.png?size=96', # Shaiiko
-          'https://cdn.discordapp.com/emojis/893376956400304128.png?size=96', # cmonBruh
-          'https://cdn.discordapp.com/emojis/894659579990921226.png?size=96', # pepeAgony
-          'https://cdn.discordapp.com/emojis/792923101179805696.png?size=96', # Pepega
-          'https://cdn.discordapp.com/emojis/792923101129474079.png?size=96', # FeelsGoodMan
-]
-
-cooldown = 0
-
-# Ubi authenticate.
-email = os.getenv('UBIEMAIL')
-password = os.getenv('UBIPASS')
-
-# Build client.
+# Build client
 client = commands.Bot(command_prefix='.')
 
-# Initialize when bot is online.
 @client.event
 async def on_ready():
-  global cooldown
-  cooldown = time.time()
+  """
+  Initialize when bot is online.
+  """
+  for guild in client.guilds:
+        for channel in guild.text_channels :
+            if str(channel) == "general" :
+                await channel.send(f'https://cdn.discordapp.com/emojis/792923101179805696.png?size=96')
+                await channel.send(f'{NAME} is online! Version: {VERSION}')
   await client.change_presence(activity=discord.Streaming(name="Rainbow Six Siege", url="https://www.twitch.tv/shaiiko"))
 
-# Introduce new member.
 @client.event
 async def on_member_join(member):
   await member.create_dm()
   await member.dm_channel.send(
-      f'Hi {member.name}, welcome and type .help for Pepega commands!'
+      f'Hi {member.name}, welcome and type .help for {NAME} commands!'
   )
 
-# Ask command. Responds with random answer to question.
 @client.command()
-async def ask(ctx, *, question):
+async def ask(ctx, question=None):
+  if question is None:
+    await ctx.send(f'https://cdn.discordapp.com/emojis/792923101179805696.png?size=96')
+    await ctx.send('Didn\'t type a question!')
+    return False
+
   await ctx.send(f'{random.choice(ASK_RESP)}')
 
-# R6 command. List user stats for current season. Optional dbg for programmers
-@client.command()
-async def r6(ctx, username=None, dbg_set=None):
-  global cooldown
-  dbg = False
-
-  # Check cooldown. Limit to one request per 5 seconds.
-  remaining_time = int(time.time() - cooldown)
-  if remaining_time < 5:
-    remaining_time = 5 - remaining_time
-    await ctx.send(f'{random.choice(EMOTES)}')
-    await ctx.send(f'Wait {remaining_time} second(s)')
-    return False
-  
-  # Check for no username.
-  if username is None:
-    await ctx.send('Didn\'t type a username Pepega!')
-    return False
-
-  if dbg_set == 'dbg':
-    dbg = True
-
-  # Reset cooldown.
-  cooldown = time.time()
-
-  # Create Ubi authentication to avoid ticket expiration.
-  ubi = UbiAuth(email=email, password=password)
-  authenticated = ubi.create_ubi_authentication()
-
-  if authenticated is False:
-    await ctx.send('Error authenticating with Ubisoft.')
-    return False
-
-  # Request user ID from username given.
-  link = f'https://public-ubiservices.ubi.com/v3/profiles?namesOnPlatform={username}&platformType=uplay'
-  resp_json = ubi.get_ubi_data(link=link)
-  if resp_json is False:
-    await ctx.send('Error retrieving player.')
-    return False
-  if dbg:
-    await ctx.send(f'[DEBUG-USER-ID] {resp_json}')
-
-  # Retrieve player's user ID.
-  player = Player()
-  try:
-    userId = resp_json['profiles'][0]['userId']
-    player.username = resp_json['profiles'][0]['nameOnPlatform']
-  except (IndexError, KeyError) as e:
-    await ctx.send('User not found.')
-    return False
-
-  # Request player's info.
-  link = f'https://public-ubiservices.ubi.com/v1/spaces/5172a557-50b5-4665-b7db-e3f2e8c5041d/sandboxes/OSBOR_PC_LNCH_A/r6karma/players?board_id=pvp_ranked&season_id=-1&region_id=ncsa&profile_ids={userId}'
-  resp_json = ubi.get_ubi_data(link)
-  if resp_json is False:
-    await ctx.send('Error retrieving player.')
-    return False
-  if dbg:
-    await ctx.send(f'[DEBUG-PLAYER] {resp_json}')
-
-  # Create player info from response.
-  player.profile_pic = f'https://ubisoft-avatars.akamaized.net/{userId}/default_146_146.png?appId=3587dcbb-7f81-457c-9781-0e3f29f6f56a'
-  player.r6_url = f'https://r6.tracker.network/profile/pc/{username}'
-  try:
-    player.rank = resp_json['players'][userId]['rank']
-    player.max_rank = resp_json['players'][userId]['max_rank']
-    player.mmr = int(resp_json['players'][userId]['mmr'])
-    player.wins = resp_json['players'][userId]['wins']
-    player.losses = int(resp_json['players'][userId]['losses'])
-    kills = resp_json['players'][userId]['kills']
-    deaths = resp_json['players'][userId]['deaths']
-  except (IndexError, KeyError) as e:
-    await ctx.send('Error retrieving player.')
-    return False
-
-  # Catch 0 deaths.
-  try:
-    player.kd =  (kills / deaths)
-  except ZeroDivisionError:
-    player.kd = kills
-
-  # Create Discord embed summary.
-  summary = player.print_summary()
-  await ctx.send(embed=summary)
-
+client.add_cog(Music(client))
+client.add_cog(R6(client))
 client.run(TOKEN)
--- ubi.py
+++ ubi.py
@@ -5,76 +5,38 @@
 # and retrieve player data and authentication.
 
 import base64
-import json
 import requests
-import discord
+import random
 import os
+import time
+import discord
+from time import strftime, gmtime
+from discord.ext import commands
 from json.decoder import JSONDecodeError
 from datetime import datetime, timedelta
 from requests.api import head
+from constants import APP_ID, RANK, RANK_ICONS, EMOTES, SEASON
 
-RANK = ["Unranked",
-        "Copper V",   "Copper IV",   "Copper III",   "Copper II",   "Copper I",
-        "Bronze V",   "Bronze IV",   "Bronze III",   "Bronze II",   "Bronze I",
-        "Silver V",   "Silver IV",   "Silver III",   "Silver II",   "Silver I",
-        "Gold III",     "Gold II",     "Gold I",
-        "Platinum III", "Platinum II", "Platinum I", 
-        "Diamond III", "Diamond II", "Diamond I",
-        "Champion"
-]
+# Ubi authenticate.
+email = os.getenv('UBIEMAIL')
+password = os.getenv('UBIPASS')
 
-RANK_ICONS = ["https://i.imgur.com/sB11BIz.png",  # unranked
-
-              "https://i.imgur.com/B8NCTyX.png",  # copper 5
-              "https://i.imgur.com/ehILQ3i.jpg",  # copper 4
-              "https://i.imgur.com/6CxJoMn.jpg",  # copper 3
-              "https://i.imgur.com/eI11lah.jpg",  # copper 2
-              "https://i.imgur.com/0J0jSWB.jpg",  # copper 1
-
-              "https://i.imgur.com/TIWCRyO.png",   # bronze 5
-              "https://i.imgur.com/42AC7RD.jpg",  # bronze 4
-              "https://i.imgur.com/QD5LYD7.jpg",  # bronze 3
-              "https://i.imgur.com/9AORiNm.jpg",  # bronze 2
-              "https://i.imgur.com/hmPhPBj.jpg",  # bronze 1
-
-              "https://i.imgur.com/PY2p17k.png",  # silver 5
-              "https://i.imgur.com/D36ZfuR.jpg",  # silver 4
-              "https://i.imgur.com/m8GToyF.jpg",  # silver 3
-              "https://i.imgur.com/EswGcx1.jpg",  # silver 2
-              "https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/4guViAQud6vSRPWoWGUNyU/32b279721dcfb09d0668f7b6a15ae3ea/R6S_RANK_500x500_Silver_01.png",    # silver 1
-
-              "https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/5o6FA0tOweqf2RMm6ly9ET/5cf7d4ce0465315dfa4012a6a84c428a/R6S_RANK_500x500_GOLD_03.png",      # gold 3
-              "https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/HQeTsDatqQRo9qW5KKs1x/4ba25d10cfb681d3347985a1125b69cf/R6S_RANK_500x500_GOLD_02.png",       # gold 2
-              "https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/8Mpi8OU4AnEh93pVcapyW/2b140e3caae994c8fa2255623cba323e/R6S_RANK_500x500_GOLD_01.png",       # gold 1
-
-              "https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/1NdqJyCZK86i3AnQwEvIlt/68ed7769e4098ec682e8c793f5121722/R6S_RANK_500x500_Platinum_03.png",  # plat 3
-              "https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/54T8y7G0Yu6qniIliDAuWz/d7a8d760969db59b1608c255e1674a65/R6S_RANK_500x500_Platinum_02.png",  # plat 2
-              "https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/70KhxbY7ZSilFgcMFIFKip/cb5435f1e0e59804f38496868d8e9369/R6S_RANK_500x500_Platinum_01.png",  # plat 1
-
-              "https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/2ynWMUUP5klIyABI8yTxyy/1bcfc19f8142f2c6b4d2e23a1f2387af/RANK_L_Diamond_03.png",             # diamond 3
-              "https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/1yXN9oMKkeAvgRFR7vZNo8/2efc56be886b09178a7af99ccab8606a/RANK_L_Diamond_02.png",             # diamond 2
-              "https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/3IjRzUddD2cTrx6xdVXFmY/49195f3e71b2d5694b50524bfd8fd30c/R6S_RANK_500x500_Diamond_01.png",   # diamond 1
-
-              "https://staticctf.akamaized.net/J3yJr34U2pZ2Ieem48Dwy9uqj5PNUQTn/74IhNF2m0VsDiurjYt9Q58/deb07d428a4a25acd70c92f6c7fcfca4/R6S_RANK_500x500_Champions_01.png"  # champion
-]
-
-# Ubi authentification using private email/password stored in env variables.
-# Methods to initialize and get data by sending GET/POST requests to Ubisoft servers.
 class UbiAuth:
-  # Initialize token authentication from email/password and save session.
-  #   Email: User's Ubisoft email.
-  #   Password: User's Ubisoft password.
+  """
+  Ubi authentification using private email/password stored in env variables.
+  Methods to initialize and get data by sending GET/POST requests to Ubisoft servers.
+  Initialize token authentication from email/password and save session.
+  """
   def __init__(self, email='', password=''):
     self.token = base64.b64encode((email + ":" + password).encode("utf-8")).decode("utf-8")
-    self.appId = '3587dcbb-7f81-457c-9781-0e3f29f6f56a'
+    self.app_id = APP_ID
 
-  # Request using authentication and save session token for use in retrieving Ubisoft profiles.
   def create_ubi_authentication(self):
     headers = {
       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
 			'Content-Type': 'application/json',
 			'Authorization': 'Basic ' + self.token,
-			'Ubi-AppId': self.appId,
+			'Ubi-AppId': self.app_id,
       'Connection': 'keep-alive'
 		}
     payload = {
@@ -95,22 +57,26 @@ class UbiAuth:
       
     try:
       self.ticket = resp_json['ticket']
-      self.sessionId = resp_json['sessionId']
-      self.sessionKey = resp_json['sessionKey']
-      self.spaceId = resp_json['spaceId']
-      self.ownUserId = resp_json['userId']
+      self.session_id = resp_json['sessionId']
+      self.session_key = resp_json['sessionKey']
+      self.space_id = resp_json['spaceId']
+      self.own_user_id = resp_json['userId']
     except (IndexError, KeyError) as e:
       print('Ubisoft authentication failed decoding session keys.')
       return False
   
-  # Request data from Ubisoft using link.
   def get_ubi_data(self, link='http://google.com'):
     headers = {
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36',
+      'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.71 Safari/537.36', 
+      "Upgrade-Insecure-Requests": "1",
+      "DNT": "1",
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+      "Accept-Language": "en-US,en;q=0.5",
+      "Accept-Encoding": "gzip, deflate",
       'Content-Type': 'application/json',
       'Authorization': 'Ubi_v1 t=' + self.ticket,
-      'Ubi-AppId': self.appId,
-      'Ubi-SessionId': self.sessionId,
+      'Ubi-AppId': self.app_id,
+      'Ubi-SessionId': self.session_id,
       'Connection': 'keep-alive',
       'expiration': f'{(datetime.now() + timedelta(hours=2)).strftime("%Y-%m-%dT%H:%M:%S")}.657Z'
     }
@@ -123,10 +89,15 @@ class UbiAuth:
       print(f'[get_data] Invalid link: {link}')
       return False
 
-# Player object contains all stats retrieved.
 class Player:
+  """
+  Store information retrieved from Ubisoft containing 
+  fields such as username, wins, losses and K/D.
+  """
   def __init__(self):
     self.username = ''
+    self.level = 0
+    self.play_time = 0
     self.profile_pic = ''
     self.r6_url = ''
     self.mmr = 0
@@ -136,16 +107,145 @@ class Player:
     self.wins = 0
     self.losses = 0
 
-  # Print player summary for current season.
   def print_summary(self):
-    embed = discord.Embed(title='Crystal Guard', colour=discord.Color.green())
+    m, s = divmod(self.play_time, 60)
+    h, m = divmod(m, 60)
+    embed = discord.Embed(title=SEASON, colour=discord.Color.green())
     embed.set_author(name=self.username, url=self.r6_url, icon_url=self.profile_pic)
     embed.set_thumbnail(url=RANK_ICONS[self.rank])
-    embed.add_field(name="Rank", value=RANK[self.rank], inline=True)
+    embed.add_field(name="Level", value=self.level, inline=True)
+    embed.add_field(name="Lifetime", value=f'{h}H', inline=True)
     embed.add_field(name="MMR", value=self.mmr, inline=True)
     embed.add_field(name="K/D", value='{:.2f}'.format(self.kd), inline=True)
     embed.add_field(name="Wins", value=self.wins, inline=True)
     embed.add_field(name="Losses", value=self.losses, inline=True)
-    embed.add_field(name="Max Rank", value=RANK[self.max_rank], inline=True)
-  
+    embed.add_field(name=f"Best {SEASON} Rank", value=RANK[self.max_rank], inline=True)
     return embed
+
+class R6(commands.Cog):
+  def __init__(self, bot):
+      self.bot = bot
+      self.ubi = UbiAuth(email=email, password=password)
+      self.authenticated = False
+      self.cooldown = 0
+      
+  @commands.command()
+  async def r6(self, ctx, username=None, dbg_set=None):
+    """
+    R6 command. List user stats for current season. Optional dbg for programmers.
+
+    Arguments:
+    ctx -- discord context.
+    username -- username to search.
+    dbg_set -- 'dbg' to display HTTP requests.
+    """
+    dbg = False
+
+    # Check cooldown. Limit to one request per 5 seconds
+    remaining_time = int(time.time() - self.cooldown)
+    if remaining_time < 5:
+      remaining_time = 5 - remaining_time
+      await ctx.send(f'{random.choice(EMOTES)}')
+      await ctx.send(f'Wait {remaining_time} second(s)')
+      return False
+    
+    # Check for no username
+    if username is None:
+      await ctx.send(f'https://cdn.discordapp.com/emojis/792923101179805696.png?size=96')
+      await ctx.send('Didn\'t type a username!')
+      return False
+
+    if dbg_set == 'dbg':
+      dbg = True
+
+    # Reset cooldown
+    self.cooldown = time.time()
+
+    # Create session if false
+    if self.authenticated is False:
+      self.authenticated = self.ubi.create_ubi_authentication()
+
+    if self.authenticated is False:
+      await ctx.send('Error authenticating with Ubisoft.')
+      return False
+
+    # Request user ID from username given
+    link = f'https://public-ubiservices.ubi.com/v3/profiles?namesOnPlatform={username}&platformType=uplay'
+    resp_json = self.ubi.get_ubi_data(link=link)
+    if resp_json is False:
+      await ctx.send('Error retrieving player.')
+      return False
+    if dbg:
+      await ctx.send(f'[DEBUG-USER-ID] {resp_json}')
+
+    # Retrieve player's user ID
+    player = Player()
+    try:
+      user_id = resp_json['profiles'][0]['userId']
+      player.username = resp_json['profiles'][0]['nameOnPlatform']
+    except (IndexError, KeyError) as e:
+      await ctx.send('User not found.')
+      return False
+
+    # Request player's info
+    link = f'https://public-ubiservices.ubi.com/v1/spaces/5172a557-50b5-4665-b7db-e3f2e8c5041d/sandboxes/OSBOR_PC_LNCH_A/r6karma/players?board_id=pvp_ranked&season_id=-1&region_id=ncsa&profile_ids={user_id}'
+    resp_json = self.ubi.get_ubi_data(link)
+    if resp_json is False:
+      await ctx.send('Error retrieving player info.')
+      return False
+    if dbg:
+      await ctx.send(f'[DEBUG-PLAYER] {resp_json}')
+
+    # Create player info from response
+    player.profile_pic = f'https://ubisoft-avatars.akamaized.net/{user_id}/default_146_146.png?appId=3587dcbb-7f81-457c-9781-0e3f29f6f56a'
+    player.r6_url = f'https://r6.tracker.network/profile/pc/{username}'
+    try:
+      player.rank = resp_json['players'][user_id]['rank']
+      player.max_rank = resp_json['players'][user_id]['max_rank']
+      player.mmr = int(resp_json['players'][user_id]['mmr'])
+      player.wins = resp_json['players'][user_id]['wins']
+      player.losses = int(resp_json['players'][user_id]['losses'])
+      kills = resp_json['players'][user_id]['kills']
+      deaths = resp_json['players'][user_id]['deaths']
+    except (IndexError, KeyError) as e:
+      await ctx.send('Error retrieving player info.')
+      return False
+
+    # Catch 0 deaths
+    try:
+      player.kd =  (kills / deaths)
+    except ZeroDivisionError:
+      player.kd = kills
+
+    # Get player level
+    link = f'https://public-ubiservices.ubi.com/v1/profiles/{user_id}/stats/ProgressionClearanceLevel?spaceId=5172a557-50b5-4665-b7db-e3f2e8c5041d'
+    resp_json = self.ubi.get_ubi_data(link)
+    if resp_json is False:
+      await ctx.send('Error retrieving player level.')
+      return False
+    if dbg:
+      await ctx.send(f'[DEBUG-PLAYER-LEVEL] {resp_json}')
+    try:
+      player.level = resp_json['stats']['ProgressionClearanceLevel']['value']
+    except (IndexError, KeyError) as e:
+      await ctx.send('Error retrieving player level.')
+      return False
+
+     # Get play time in seconds
+    link = f'https://public-ubiservices.ubi.com/v1/profiles/stats?profileIds={user_id}&spaceId=5172a557-50b5-4665-b7db-e3f2e8c5041d&statNames=ProgressionPvPTimePlayed'
+    resp_json = self.ubi.get_ubi_data(link)
+    if resp_json is False:
+      await ctx.send('Error retrieving play time.')
+      return False
+    if dbg:
+      await ctx.send(f'[DEBUG-PLAY-TIME] {resp_json}')
+    try:
+      player.play_time = int(resp_json['profiles'][0]['stats']['ProgressionPvPTimePlayed']['value'])
+    except (IndexError, KeyError) as e:
+      await ctx.send('Error retrieving play time.')
+      return False
+
+    # Create Discord embed summary
+    async with ctx.typing():
+      summary = player.print_summary()
+    await ctx.send(embed=summary)
